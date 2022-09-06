9750e278f959954458a70ea32fcb0a584ab0dc97
iradukud <iradukuda10@gmail.com>
2022-09-06 16:57:56 -0500

    added more views, routes and controllers

--- routes/main.js
+++ routes/main.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const router = express.Router()
-const authController = require('../controllers/auth') 
-const homeController = require('../controllers/home')
+const authController = require('../controllers/auth')
+const mainController = require('../controllers/main')
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
-router.get('/', homeController.getIndex)
+router.get('/', mainController.getIndex)
 router.get('/login', authController.getLogin)
 router.post('/login', authController.postLogin)
 router.get('/logout', authController.logout)
 router.get('/signup', authController.getSignup)
 router.post('/signup', authController.postSignup)
+
+router.get('/welcome', ensureAuth, mainController.getWelcome)
+router.get('/create', ensureAuth, mainController.getCreation)
+router.get('/missions', ensureAuth, mainController.getMissions)
 
 module.exports = router
