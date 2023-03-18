9704890928aaa3241213b0be201c404a59997559
iradukud <iradukuda10@gmail.com>
2023-03-17 20:13:53 -0500

    refactoring
    - created partials
    - fixed some routing errors

--- routes/main.js
+++ routes/main.js
@@ -8,12 +8,12 @@ const mainController = require('../controllers/main')
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 //Routes that deal with logining in
-router.get('/', ensureAuth, mainController.getIndex)
-router.get('/login', ensureAuth, authController.getLogin)
-router.post('/login', ensureAuth, authController.postLogin)
-router.get('/logout', ensureAuth, authController.logout)
-router.get('/signup', ensureAuth, authController.getSignup)
-router.post('/signup', ensureAuth, authController.postSignup)
+router.get('/', mainController.getIndex)
+router.get('/login',  authController.getLogin)
+router.post('/login', authController.postLogin)
+router.get('/logout', authController.logout)
+router.get('/signup', authController.getSignup)
+router.post('/signup', authController.postSignup)
 
 //Routes that generate a page
 router.get('/welcome', ensureAuth, mainController.getWelcome)
