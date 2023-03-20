ba2fa0031bf01a0acd1a6eefd5555e6af5e4782b
iradukud <iradukuda10@gmail.com>
2023-03-20 10:15:20 -0500

    worked_on_homepage

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,13 +4,11 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/auth')
-const mainController = require('../controllers/main')
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 //Routes that deal with logining in
 router.post('/login', authController.postLogin)
 router.get('/logout', authController.logout)
 router.post('/signup', authController.postSignup)
-
 
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -4,12 +4,10 @@ const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
-const methodOverride = require("method-override")
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
-const mainRoutes = require('./routes/main')
-const missionRoutes = require('./routes/missions')
+const routes = require('./routes/api')
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
@@ -20,21 +18,12 @@ require('./config/passport')(passport)
 //Connect To Database
 connectDB()
 
-//Using EJS for views
-app.set("view engine", "ejs")
-
-//Static Folder
-app.use(express.static("public"))
-
 //Body Parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
 //Logging
 app.use(logger("dev"))
-
-//Use forms for put / delete
-app.use(methodOverride("_method"))
 
 // Setup Sessions - stored in MongoDB
 app.use(
@@ -54,8 +43,7 @@ app.use(passport.session())
 app.use(flash())
 
 //Setup Routes For Which The Server Is Listening
-app.use('/', mainRoutes)
-app.use('/mission', missionRoutes)
+app.use('/api', routes)
 
 //Server Running
 app.listen(process.env.PORT, () => {
