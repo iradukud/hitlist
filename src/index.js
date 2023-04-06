4b978070facdd467d7e7b8f385879e689b09cea4
iradukud <iradukuda10@gmail.com>
2023-04-06 16:20:38 -0500

    changed_pck_proxy
    - changed proxy for each api request

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+//stylesheets
 import './index.css';
+//components
 import App from './App';
+//constext
 import { MissionsContextProvider } from './context/MissionContext';
 import { AuthContextProvider } from './context/AuthContext';
 
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
