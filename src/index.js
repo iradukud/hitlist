b86c496a795da835753255a50ec9ef9dcf70c66a
iradukud <iradukuda10@gmail.com>
2023-04-06 22:07:00 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { MissionsContextProvider } from './context/MissionContext';
 import { AuthContextProvider } from './context/AuthContext';
 
 if (process.env.NODE_ENV === 'production') disableReactDevTools()
-s
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
